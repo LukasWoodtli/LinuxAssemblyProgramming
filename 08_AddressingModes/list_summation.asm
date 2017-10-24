5c01fdb572f3634fb5a0f1dd0560c89c129d2a77
Lukas Woodtli <woodtli.lukas@gmail.com>
2017-10-24 21:38:05 +0200

    Add example for memory addressing

---
+++ 08_AddressingModes/list_summation.asm
@@ -0,0 +1,33 @@
+; sum of a list of numbers
+
+
+section .data
+
+EXIT_SUCCESS    equ     0
+SYS_exit        equ    60
+
+section .data
+    lst     dd     1002, 1004, 1006, 1008, 10010
+    len     dd     5
+    sum     dd     0
+
+
+section .text
+global _start
+_start:
+
+    mov ecx, dword [len]
+    mov rsi, 0
+    
+sumLoop:
+    mov eax, dword [lst + rsi * 4]  ; equivalent to lst[rsi]
+    add dword [sum], eax
+    inc rsi
+    loop sumLoop
+
+
+last:
+    mov rax, SYS_exit
+    mov rdi, EXIT_SUCCESS
+    syscall
+    
